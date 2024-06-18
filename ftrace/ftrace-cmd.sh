586869e56fbc3bcb13f924b90ad08b83ac149bab
RinHizakura <s921975628@gmail.com>
2024-06-18 13:11:35 +0800

    Fix ftrace-cmd to fit older versioned kernel

--- ftrace/ftrace-cmd.sh
+++ ftrace/ftrace-cmd.sh
@@ -7,16 +7,25 @@ OUTPUT=/tmp/trace_log
 function root_run()
 {
     sudo sh -c "$1"
+
+    ret=$?
+    if [ $ret -eq 1 ]
+    then
+        echo "Run command $1 failed."
+        exit 1
+    fi
 }
 
-cd $SYSFS_TRACE
+# Clean the trace buffer at start
+root_run "echo 0 > $SYSFS_TRACE/trace"
 
 # Disable the trace first before we setup everything
-root_run "echo 0 > tracing_on"
-root_run "echo 0 > trace"
+root_run "echo 0 > $SYSFS_TRACE/tracing_on"
 
-# Choose the function tracer
-root_run "echo function > current_tracer"
+# Choose the tracer with target setting
+root_run "echo 1 >  $SYSFS_TRACE/events/enable"
+root_run "echo event-fork > $SYSFS_TRACE/trace_options"
+root_run "echo function > $SYSFS_TRACE/current_tracer"
 
 # Enable trace and start running the command
 (sleep 5; $CMD) &
@@ -24,17 +33,17 @@ CPID=$!
 echo "Run command '$CMD'(ppid=$$ pid=$CPID) and enable tracing..."
 
 # Add child pid to filter to start tracing it
-root_run "echo function-fork > trace_options"
-root_run "echo $CPID > set_ftrace_pid"
-root_run "echo 1 > tracing_on"
+root_run "echo $CPID > $SYSFS_TRACE/set_event_pid"
+root_run "echo 1 > $SYSFS_TRACE/tracing_on"
 wait $CPID
-root_run "echo 0 > tracing_on"
+root_run "echo 0 > $SYSFS_TRACE/tracing_on"
 
 # Output result
-root_run "cat trace > $OUTPUT"
+root_run "cat $SYSFS_TRACE/trace > $OUTPUT"
 echo "Done. Please 'sudo cat $OUTPUT' for the result"
 
-# Cleanup the change of ftrace-cmd
-root_run "echo nop > current_tracer"
-root_run "echo > set_ftrace_pid"
-root_run "echo nofunction-fork > trace_options"
+# Cleanup the change of ftrace
+root_run "echo > $SYSFS_TRACE/set_event_pid"
+root_run "echo nop > $SYSFS_TRACE/current_tracer"
+root_run "echo noevent-fork > $SYSFS_TRACE/trace_options"
+root_run "echo 0 >  $SYSFS_TRACE/events/enable"
