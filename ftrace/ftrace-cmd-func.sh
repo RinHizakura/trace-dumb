be70eca9543eb4550ded0327ffe49eb54ba15725
RinHizakura <s921975628@gmail.com>
2024-07-10 23:20:52 +0800

    Refactor ftrace-cmd-func

--- ftrace/ftrace-cmd-func.sh
+++ ftrace/ftrace-cmd-func.sh
@@ -3,17 +3,10 @@
 SYSFS_TRACE=/sys/kernel/debug/tracing
 OUTPUT=/tmp/trace_log
 
-function root_run()
-{
-    sudo sh -c "$1"
-
-    ret=$?
-    if [ $ret -ne 0 ]
-    then
-        echo "Run command $1 failed."
-        exit 1
-    fi
-}
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root user."
+    exit 1
+fi
 
 FUNC=
 TRACE=
@@ -40,19 +33,19 @@ CMD=$*
 
 
 # Clean the trace buffer at start
-root_run "echo 0 > $SYSFS_TRACE/trace"
+echo 0 > $SYSFS_TRACE/trace
 
 # Disable the trace first before we setup everything
-root_run "echo 0 > $SYSFS_TRACE/tracing_on"
+echo 0 > $SYSFS_TRACE/tracing_on
 
 # Choose the tracer with target setting
-root_run "echo $TRACER > $SYSFS_TRACE/current_tracer"
-root_run "echo $FUNC > $SYSFS_TRACE/set_graph_function"
-root_run "echo $FUNC > $SYSFS_TRACE/set_ftrace_filter"
-root_run "echo $TRACE >> $SYSFS_TRACE/set_ftrace_filter"
-root_run "echo $NOTRACE > $SYSFS_TRACE/set_ftrace_notrace"
-root_run "echo 1 > $SYSFS_TRACE/options/function-fork"
-root_run "echo 1 > $SYSFS_TRACE/options/funcgraph-tail"
+echo $TRACER > $SYSFS_TRACE/current_tracer
+echo $FUNC > $SYSFS_TRACE/set_graph_function
+echo $FUNC > $SYSFS_TRACE/set_ftrace_filter
+echo $TRACE >> $SYSFS_TRACE/set_ftrace_filter
+echo $NOTRACE > $SYSFS_TRACE/set_ftrace_notrace
+echo 1 > $SYSFS_TRACE/options/function-fork
+echo 1 > $SYSFS_TRACE/options/funcgraph-tail
 
 # Enable trace and start running the command
 (sleep 5; $CMD) &
@@ -60,20 +53,20 @@ CPID=$!
 echo "Run command '$CMD'(ppid=$$ pid=$CPID) and enable tracing..."
 
 # Add child pid to filter to start tracing it
-root_run "echo $CPID > $SYSFS_TRACE/set_ftrace_pid"
-root_run "echo 1 > $SYSFS_TRACE/tracing_on"
+echo $CPID > $SYSFS_TRACE/set_ftrace_pid
+echo 1 > $SYSFS_TRACE/tracing_on
 wait $CPID
-root_run "echo 0 > $SYSFS_TRACE/tracing_on"
+echo 0 > $SYSFS_TRACE/tracing_on
 
 # Output result
-root_run "cat $SYSFS_TRACE/trace > $OUTPUT"
+cat $SYSFS_TRACE/trace > $OUTPUT
 echo "Done. Please 'sudo cat $OUTPUT' for the result"
 
 # Cleanup the change of ftrace
-root_run "echo > $SYSFS_TRACE/set_ftrace_pid"
-root_run "echo 0 > $SYSFS_TRACE/options/funcgraph-tail"
-root_run "echo 0 > $SYSFS_TRACE/options/function-fork"
-root_run "echo > $SYSFS_TRACE/set_ftrace_notrace"
-root_run "echo > $SYSFS_TRACE/set_ftrace_filter"
-root_run "echo > $SYSFS_TRACE/set_graph_function"
-root_run "echo nop > $SYSFS_TRACE/current_tracer"
+echo > $SYSFS_TRACE/set_ftrace_pid
+echo 0 > $SYSFS_TRACE/options/funcgraph-tail
+echo 0 > $SYSFS_TRACE/options/function-fork
+echo > $SYSFS_TRACE/set_ftrace_notrace
+echo > $SYSFS_TRACE/set_ftrace_filter
+echo > $SYSFS_TRACE/set_graph_function
+echo nop > $SYSFS_TRACE/current_tracer
