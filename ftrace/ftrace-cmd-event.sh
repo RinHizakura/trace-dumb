826f461014a3dc4cd503d8b33e2cc7554ffb6cf7
RinHizakura <s921975628@gmail.com>
2024-07-10 23:18:06 +0800

    Refactor ftrace-cmd-event

--- ftrace/ftrace-cmd-event.sh
+++ ftrace/ftrace-cmd-event.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
-CMD=$@
 SYSFS_TRACE=/sys/kernel/debug/tracing
 OUTPUT=/tmp/trace_log
 
-function root_run()
-{
-    sudo sh -c "$1"
-
-    ret=$?
-    if [ $ret -eq 1 ]
-    then
-        echo "Run command $1 failed."
-        exit 1
-    fi
-}
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root user."
+    exit 1
+fi
 
 EVENT=$SYSFS_TRACE/events
 while getopts ":e:" opt
@@ -30,19 +23,18 @@ done
 shift $(($OPTIND - 1))
 CMD=$*
 
-
 # Clean the trace buffer at start
-root_run "echo 0 > $SYSFS_TRACE/trace"
+echo 0 > $SYSFS_TRACE/trace
 
 # Disable the trace first before we setup everything
-root_run "echo 0 >  $SYSFS_TRACE/events/enable"
-root_run "echo 0 > $SYSFS_TRACE/tracing_on"
+echo 0 > $SYSFS_TRACE/events/enable
+echo 0 > $SYSFS_TRACE/tracing_on
 
 # Choose the tracer with target setting
-root_run "echo 1 >  $EVENT/enable"
-root_run "echo event-fork > $SYSFS_TRACE/trace_options"
-root_run "echo latency-format > $SYSFS_TRACE/trace_options"
-root_run "echo nop > $SYSFS_TRACE/current_tracer"
+echo 1 >  $EVENT/enable
+echo event-fork > $SYSFS_TRACE/trace_options
+echo latency-format > $SYSFS_TRACE/trace_options
+echo nop > $SYSFS_TRACE/current_tracer
 
 # Enable trace and start running the command
 (sleep 5; $CMD) &
@@ -50,18 +42,18 @@ CPID=$!
 echo "Run command '$CMD'(ppid=$$ pid=$CPID) and enable tracing..."
 
 # Add child pid to filter to start tracing it
-root_run "echo $CPID > $SYSFS_TRACE/set_event_pid"
-root_run "echo 1 > $SYSFS_TRACE/tracing_on"
+echo $CPID > $SYSFS_TRACE/set_event_pid
+echo 1 > $SYSFS_TRACE/tracing_on
 wait $CPID
-root_run "echo 0 > $SYSFS_TRACE/tracing_on"
+echo 0 > $SYSFS_TRACE/tracing_on
 
 # Output result
-root_run "cat $SYSFS_TRACE/trace > $OUTPUT"
+cat $SYSFS_TRACE/trace > $OUTPUT
 echo "Done. Please 'sudo cat $OUTPUT' for the result"
 
 # Cleanup the change of ftrace
-root_run "echo > $SYSFS_TRACE/set_event_pid"
-root_run "echo nop > $SYSFS_TRACE/current_tracer"
-root_run "echo nolatency-format > $SYSFS_TRACE/trace_options"
-root_run "echo noevent-fork > $SYSFS_TRACE/trace_options"
-root_run "echo 0 >  $EVENT/enable"
+echo > $SYSFS_TRACE/set_event_pid
+echo nop > $SYSFS_TRACE/current_tracer
+echo nolatency-format > $SYSFS_TRACE/trace_options
+echo noevent-fork > $SYSFS_TRACE/trace_options
+echo 0 >  $EVENT/enable
