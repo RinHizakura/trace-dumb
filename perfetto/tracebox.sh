a1ba5b7d6f147769df06d7be8b42cb04903a00c7
RinHizakura <s921975628@gmail.com>
2024-10-15 19:35:26 +0800

    Replace trace script by a general one

---
+++ perfetto/tracebox.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SYSFS_TRACE=/sys/kernel/debug/tracing
+TRACEBOX=./tracebox
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root user."
+    exit 1
+fi
+
+while getopts ":c:" opt
+do
+    case $opt in
+        c)
+            CFG=$OPTARG;;
+        ?)
+            exit 1;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+CMD=$*
+
+$TRACEBOX -o trace_file.perfetto-trace --txt -c $CFG &
+TPID=$!
+
+(sleep 5;$CMD) &
+CPID=$!
+
+echo "Run command '$CMD'(ppid=$$ pid=$CPID) and enable tracing..."
+wait $CPID
+wait $TPID
+echo "Done!"
