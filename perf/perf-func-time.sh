6e71d698d251f98081017f2f789b5de422fe50cc
RinHizakura <s921975628@gmail.com>
2024-10-31 00:19:05 +0800

    Add script to log and parse perf-probe data

---
+++ perf/perf-func-time.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+PERF=perf
+OUTPUT=/tmp/perf_result
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root user."
+    exit 1
+fi
+
+FUNC=
+while getopts ":f:" opt
+do
+    case $opt in
+        f)
+            FUNC=$OPTARG;;
+        ?)
+            exit 1;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+CMD=$*
+
+perf probe --add $FUNC
+perf probe --add "${FUNC}%return"
+perf record -e probe:${FUNC}* $CMD
+# Output result
+perf script -F "comm,pid,tid,cpu,time,event" > $OUTPUT
+echo "Done. Please 'sudo cat $OUTPUT' for the result"
+
+perf probe --del=probe:${FUNC}
+perf probe --del=probe:${FUNC}__return
---
+++ perf/perf_stat.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import argparse, re
+import pandas as pd
+import numpy as np
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    args = parser.parse_args()
+    return args
+
+
+header = ["comm", "pid/tid", "cpu", "time", "event"]
+
+args = get_args()
+input_file = args.input
+
+# Parse output file to know the function execution time by
+# the entry and exit timestamp
+data = []
+entry_time = {}
+with open(input_file, "r") as file:
+    for line in file:
+        l = line.strip()
+        items = re.split(r"\s+", l)
+
+        ptid = items[0] + items[1]
+        func = re.search(r":\w+:", items[4])[0][1:-1]
+        # Use us unit
+        time = float(items[3][:-1]) * 10**6
+
+        if len(func) >= 8 and func[-8:] == "__return":
+            data.append(time - entry_time[ptid])
+            entry_time.pop(ptid)
+        else:
+            assert entry_time.get(ptid) == None
+            entry_time[ptid] = time
+
+data = np.array(data)
+
+# Show stat on the target CPU
+print(f"min={np.min(data)}us, mean={np.mean(data)}us, max={np.max(data)}us")
