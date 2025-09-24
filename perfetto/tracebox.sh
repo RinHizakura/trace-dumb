b74dec3dd0b9d3cbfdc1accc081b6d0deabc5626
RinHizakura <s921975628@gmail.com>
2025-09-24 15:40:55 +0800

    Update tracebox script for better command check

--- perfetto/tracebox.sh
+++ perfetto/tracebox.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 SYSFS_TRACE=/sys/kernel/debug/tracing
 TRACEBOX=./tracebox
+
+function print_help()
+{
+    usage="$(basename "$0") [-h] [-c config] cmdline   \n
+where:                                                 \n
+    -h  show this help text                            \n
+    -c  select the config for tracebox"
+
+    echo -e $usage
+}
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root user."
@@ -13,13 +25,32 @@ do
     case $opt in
         c)
             CFG=$OPTARG;;
+        h)
+            print_help; exit 0;;
         ?)
-            exit 1;;
+            print_help; exit 1;;
     esac
 done
 
 shift $(($OPTIND - 1))
 CMD=$*
+
+if [ ! -f "$TRACEBOX" ]; then
+    echo "'$TRACEBOX' does not exist"
+    exit 1
+fi
+
+if [ -z "$CFG" ]; then
+    echo "Config '$CFG' does not exist"
+    print_help
+    exit 1
+fi
+
+if [ "$CMD" == "" ]; then
+    echo "Please specific the command to run"
+    print_help
+    exit 1
+fi
 
 $TRACEBOX -o trace_file.perfetto-trace --txt -c $CFG &
 TPID=$!
